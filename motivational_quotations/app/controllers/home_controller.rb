d475ad1d1642b6e2e73faa484572fb8cb68da5e3
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-27 15:23:19 -0500

    add PICTURES hash to Quote model; add image_tag to dashboard

--- motivational_quotations/app/controllers/home_controller.rb
+++ motivational_quotations/app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def dashboard
     @person = params[:person] || "Albert Einstein"
     @quote = Quote.get_quote(@person)
+    @picture = Quote.get_picture_url(@person)
   end
 end
--- motivational_quotations/app/models/quote.rb
+++ motivational_quotations/app/models/quote.rb
@@ -9,13 +9,22 @@ class Quote
                                               'A child playing air-guitar plays no wrong notes.',
                                               "There is only one reason that you ever fail at anything...and that is because you eventually change your mind. That's it!...anything and everything you have ever decided to do, you have succeeded, or will succeed, at doing.",
                                               "It's time for you to take control of your Life and for you to accept that it is you who's in control."],
-                          "Benjamin Franklin" => ["Wine is constant proof that God loves us and loves to see us happy.",
-                                                  "Do not fear mistakes. You will know failure. Continue to reach out.",
-                                                  "Any fool can criticize, condemn and complain - and most fools do.",
-                                                  "Be at war with your vices, at peace with your neighbors, and let every new year find you a better man.",
-                                                  "Beware of little expenses. A small leak will sink a great ship."]
+                                              "Benjamin Franklin" => ["Wine is constant proof that God loves us and loves to see us happy.",
+                                                                      "Do not fear mistakes. You will know failure. Continue to reach out.",
+                                                                      "Any fool can criticize, condemn and complain - and most fools do.",
+                                                                      "Be at war with your vices, at peace with your neighbors, and let every new year find you a better man.",
+                                                                      "Beware of little expenses. A small leak will sink a great ship."]
+  }
+
+  PICTURES = {"Albert Einstein" => "http://content8.flixster.com/rtactor/42/23/42230_pro.jpg",
+              "Victor Wooten" => "http://playcajon.org/wp-content/uploads/2014/03/wootenarticle.jpg",
+              "Benjamin Franklin" => "http://www.npg.si.edu/exhibit/legacy/slideshow/gallery/album1/large/lgben.jpg"
   }
   def self.get_quote(string)
     QUOTES[string].sample
+  end
+
+  def self.get_picture_url(string)
+    PICTURES[string]
   end
 end
